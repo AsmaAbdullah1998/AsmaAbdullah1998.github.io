08cbae898ecf788da69cb7d28f55eb809548f46f
Asma Abdullah <asarouji0002@stu.kau.edu.sa>
2021-06-12 19:20:51 +0300

    dd

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
 
-    
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
 
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-/* scroll to enter*/
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-$('.js--scroll-to-enter').click(function () {
-    $('html, body').animate({scrollTop: $('.js--section-enter').offset().top}, 1000); 
- });
- 
- 
-
-
-});
-
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
 
