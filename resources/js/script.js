92e8e4e343891440d97bdd47dc6f0e41f8b32da5
Asma Abdullah <asarouji0002@stu.kau.edu.sa>
2021-06-12 13:19:53 +0300

    ubload

---
+++ resources/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    
+
+
+/* scroll to enter*/
+
+$('.js--scroll-to-enter').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-enter').offset().top}, 1000); 
+ });
+ 
+ 
+
+
+});
+
+
+
