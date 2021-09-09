8ac37dfa2cd9e62f5a7e4771d134e43e9457fafe
Asma Abdullah <asarouji0002@stu.kau.edu.sa>
2021-09-10 00:49:31 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "esp_demo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO esp_demo_table (Sensor_Value)
+VALUES (1)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ request.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "esp_demo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT Sensor_Value FROM esp_demo_table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Sensor_Value " . $row["Sensor_Value"].  "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
